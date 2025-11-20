9b8db4777a9d2d3466ec499bd333d23fe46029fa
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-11-20 18:40:23 -0300

    feat: criando LinkButton e configurando navegação

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+defineProps<{
+    label: string;
+    routeName: string;
+}>();
+</script>
+
+<template>
+    <a class="poppins-regular" @click="router.push({name: routeName})">{{ label }}</a>
+</template>
+
+<style scoped>
+a {
+ color: var(--primary-color);
+ text-decoration: underline;
+}
+
+a:hover {
+    cursor: pointer;
+}
+
+a:hover {
+    color: var(--secondary-color);
+}
+</style>
