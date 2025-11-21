cba8945464ed669a38754b99eb09f90da8b6a49d
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-11-20 22:31:08 -0300

    feat: criando componente IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+
+defineProps<{
+    click?: () => void;
+    secondary?: boolean;
+    icon: Component;
+}>();
+</script>
+
+<template>
+    <button :class="{ 'btn-secondary': secondary }" class="poppins-medium">
+        <component :is="icon" size="25" :color="secondary ? 'var(--primary-color)' : 'var(--font-button-primary)'"/>
+    </button>
+</template>
+
+<style scoped>
+button {
+    width: 45px;
+    height: 45px;
+
+    background-color: var(--primary-color);
+    color: var(--font-button-primary);
+    border-radius: 8px;
+    border: none;
+    transition: 0.5s;
+}
+
+button:hover {
+    transition: 0.5s;
+    cursor: pointer;
+    background-color: var(--button-hover);
+}
+
+.btn-secondary {
+    background-color: var(--button-secondary);
+    color: var(--font-color);
+    border: 1px solid;
+    border-color: var(--primary-color);
+}
+
+.btn-secondary:hover {
+    background-color: #e2e2e2;
+    color: var(--font-button-primary);
+}
+</style>
+
