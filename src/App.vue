3a9b9f0bd56fbd3da03aa227e7a633e706c14652
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-11-20 21:56:20 -0300

    feat: adicionando phosphor icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import CustomTextarea from './components/CustomTextarea.vue';
 </script>
 
 <template>
-    <CustomTextarea label = "Teste"/>
 </template>
 
 <style scoped>
